0d9f75d8fc710a4594d8f4bdc19bc59a3301284d
Nicolas Finkelstein <nicolasfinkelstein2020@u.northwestern.edu>
2019-03-17 21:29:53 -0500

    updated parse_source

--- demoui/src/App.js
+++ demoui/src/App.js
@@ -2,16 +2,47 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Navbar from "./components/Navbar";
-import Source from "./components/Source";
-import MediaCard from "./components/MediaCard";
 import SourceList from "./components/SourceList";
+import Typography from "@material-ui/core/Typography";
+
+import sources from "./sources";
+import targets from "./targets";
+import mosaics from "./mosaics";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <SourceList />
+        <Typography
+          variant="h2"
+          color="primary"
+          align="center"
+          style={{ paddingTop: 20 }}
+        >
+          Target Audio
+        </Typography>
+        <SourceList sources={targets} />
+
+        <Typography
+          variant="h2"
+          color="primary"
+          align="center"
+          style={{ paddingTop: 20 }}
+        >
+          Source Audio
+        </Typography>
+        <SourceList sources={sources} />
+
+        <Typography
+          variant="h2"
+          color="primary"
+          align="center"
+          style={{ paddingTop: 20 }}
+        >
+          Audio Mosaics
+        </Typography>
+        <SourceList sources={mosaics} />
       </div>
     );
   }
--- demoui/src/components/SourceList.js
+++ demoui/src/components/SourceList.js
@@ -1,23 +1,24 @@
 import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
 import MediaCard from "./MediaCard";
-import sources from "../sources";
 
 class SourceList extends Component {
-  state = {
-    sources: sources
-  };
-
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   render() {
     return (
       <div>
-        <Grid container spacing={24} style={{ padding: 24 }}>
-          {this.state.sources.map(currentSource => (
-            <Grid item xs={12} sm={6} lg={4} xl={3}>
+        <Grid
+          alignItems="center"
+          justify="center"
+          container
+          spacing={24}
+          style={{ padding: 24 }}
+        >
+          {this.props.sources.map(currentSource => (
+            <Grid item xs={12} sm={6} lg={3} xl={3}>
               <MediaCard fields={currentSource} />
             </Grid>
           ))}
